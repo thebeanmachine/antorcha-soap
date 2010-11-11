eb766399076ae9e4b6fb0b47e3898b85f90e95da
Robin Janse <r.janse@thebeanmachine.nl>
2010-11-11 17:36:56 +0100

    delayed_job

--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101109141641) do
+ActiveRecord::Schema.define(:version => 20101111132941) do
 
   create_table "antorchas", :force => true do |t|
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
 end

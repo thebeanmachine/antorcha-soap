424f58a00753794917b5c0365e9b18e7d46f8468
Robin Janse <r.janse@thebeanmachine.nl>
2010-11-15 14:28:43 +0100

    should be notified.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,7 @@ class NotificationsController < ApplicationController
   
   def create    
     Delayed::Job.enqueue(Jobs::FetchNewMessagesJob.new)
+    render :text => "200"
   end
 
 end
--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -2,10 +2,12 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe NotificationsController do
   
-  context "with GET create"
-  it "should create a new delayed job" do
-    Delayed::Job.should_receive(:enqueue).with(Jobs::FetchNewMessagesJob.new)
-    post :create
+  context "with GET create" do
+    it "should create a new delayed job" do
+      mock_delayed_job = mock_model(Delayed::Job)
+      mock_delayed_job.stub(:enqueue).with(Jobs::FetchNewMessagesJob)      
+      post :create
+      response.should have_text("200")
+    end
   end
-  
 end

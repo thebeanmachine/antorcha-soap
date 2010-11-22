f2f4c6d01704b2c86ce8baf94db5716ba5da6e88
Robin Janse <r.janse@thebeanmachine.nl>
2010-11-22 10:22:51 +0100

    Soap envelope header and body changed

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -13,53 +13,38 @@ class Operation < ActiveRecord::Base
     # This is a test ZVJ SOAP envelope! It's static but we will make it dynamic really really soon! Oh yeah btw: ...SOAP SUCKS :D
     soap_data = <<-EOF
       <?xml version="1.0" encoding="UTF-8"?>
-      <SOAP-ENV:Envelope
-        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-        SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"
-        xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
-        
-        <SOAP-ENV:Body>
-        
-          <zvj:SignaleringRequest xmlns:zvj="http://model.zvj.nl/signalering/">
-            <Organisatie>
-              <zvj:OrganisatieType>
-                <Organisatie>Thorax</Organisatie>
-                <OrganisatiePostcode>3800AD</OrganisatiePostcode>
-                <Gebruiker>Snagel</Gebruiker>
-              </zvj:OrganisatieType>
-            </Organisatie>
-            <BurgerServiceNummer>
-              <zvj:BurgerServiceNummerType>000000000</zvj:BurgerServiceNummerType>
-            </BurgerServiceNummer>
-            <GeboorteDatum>11/11/2001</GeboorteDatum>
-            <Jongere>
-              <zvj:JongereType>
-                <Voorletters>ABC</Voorletters>
-                <Jongereadres>
-                  <zvj:JongereAdresType>
-                    <Postcodecijfers>
-                      <zjv:PostcodeCijfersType>0000</zjv:PostcodeCijfersType>
-                    </Postcodecijfers>
-                    <Postcodeletters>
-                      <zjv:PostcodeLettersType>AA</zjv:PostcodeLettersType>
-                    </Postcodeletters>
-                    <Huisnummer>
-                      100
-                    </Huisnummer>
-                  </zvj:JongereAdresType>
-                </Jongereadres>
-              </zvj:JongereType>
-            </Jongere>
-            <SignaalType>
-              <zjv:SignaalType>2</zvj:SignaalType>
-            </SignaalType>
-            <mutatienummer>123123123</mutatienummer>
-          </zvj:SignaleringRequest>
-  
-        </SOAP-ENV:Body>
-      </SOAP-ENV:Envelope>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sig="http://zvj.interaccess.nl/signalering/" xmlns:com="http://zvj.interaccess.nl/common/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <sig:nieuweSignalering>
+         <sig:OrganisatieGebruiker>
+            <com:OrganisatieNAW>
+               <com:OrganisatieNaam>Thorax</com:OrganisatieNaam>
+               <com:OrganisatiePostcode>3800AD</com:OrganisatiePostcode>
+            </com:OrganisatieNAW>
+            <com:GebruikernaamMedewerker>snagel</com:GebruikernaamMedewerker>
+         </sig:OrganisatieGebruiker>
+         <sig:VerifieerJongere>
+            <!--You have a CHOICE of the next 2 items at this level-->
+            <com:BurgerServiceNummer>123456789</com:BurgerServiceNummer>
+
+            <!--com:Jongere>
+               <!--Optional:- ->
+               <com:Achternaam>?</com:Achternaam>
+               <com:Geboortedatum>?</com:Geboortedatum>
+               <com:Geslacht>?</com:Geslacht>
+               <com:Postcode>?</com:Postcode>
+               <com:Huisnummer>?</com:Huisnummer>
+            </com:Jongere-->
+         </sig:VerifieerJongere>
+         <sig:NieuwSignaal>
+            <sig:SignaalType>4</sig:SignaalType>
+            <!--Optional:- ->
+            <sig:Einddatum>?</sig:Einddatum>-->
+         </sig:NieuwSignaal>
+      </sig:nieuweSignalering>
+   </soapenv:Body>
+</soapenv:Envelope>
     EOF
     
     # Set Headers
@@ -98,53 +83,38 @@ class Operation < ActiveRecord::Base
     def put_xml
     soap_data = <<-EOF
       <?xml version="1.0" encoding="UTF-8"?>
-      <SOAP-ENV:Envelope
-        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-        SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"
-        xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
-        
-        <SOAP-ENV:Body>
-        
-          <zvj:SignaleringRequest xmlns:zvj="http://model.zvj.nl/signalering/">
-            <Organisatie>
-              <zvj:OrganisatieType>
-                <Organisatie>Thorax</Organisatie>
-                <OrganisatiePostcode>3800AD</OrganisatiePostcode>
-                <Gebruiker>Snagel</Gebruiker>
-              </zvj:OrganisatieType>
-            </Organisatie>
-            <BurgerServiceNummer>
-              <zvj:BurgerServiceNummerType>000000000</zvj:BurgerServiceNummerType>
-            </BurgerServiceNummer>
-            <GeboorteDatum>11/11/2001</GeboorteDatum>
-            <Jongere>
-              <zvj:JongereType>
-                <Voorletters>ABC</Voorletters>
-                <Jongereadres>
-                  <zvj:JongereAdresType>
-                    <Postcodecijfers>
-                      <zjv:PostcodeCijfersType>0000</zjv:PostcodeCijfersType>
-                    </Postcodecijfers>
-                    <Postcodeletters>
-                      <zjv:PostcodeLettersType>AA</zjv:PostcodeLettersType>
-                    </Postcodeletters>
-                    <Huisnummer>
-                      100
-                    </Huisnummer>
-                  </zvj:JongereAdresType>
-                </Jongereadres>
-              </zvj:JongereType>
-            </Jongere>
-            <SignaalType>
-              <zjv:SignaalType>2</zvj:SignaalType>
-            </SignaalType>
-            <mutatienummer>123123123</mutatienummer>
-          </zvj:SignaleringRequest>
-  
-        </SOAP-ENV:Body>
-      </SOAP-ENV:Envelope>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sig="http://zvj.interaccess.nl/signalering/" xmlns:com="http://zvj.interaccess.nl/common/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <sig:nieuweSignalering>
+         <sig:OrganisatieGebruiker>
+            <com:OrganisatieNAW>
+               <com:OrganisatieNaam>Thorax</com:OrganisatieNaam>
+               <com:OrganisatiePostcode>3800AD</com:OrganisatiePostcode>
+            </com:OrganisatieNAW>
+            <com:GebruikernaamMedewerker>snagel</com:GebruikernaamMedewerker>
+         </sig:OrganisatieGebruiker>
+         <sig:VerifieerJongere>
+            <!--You have a CHOICE of the next 2 items at this level-->
+            <com:BurgerServiceNummer>123456789</com:BurgerServiceNummer>
+
+            <!--com:Jongere>
+               <!--Optional:- ->
+               <com:Achternaam>?</com:Achternaam>
+               <com:Geboortedatum>?</com:Geboortedatum>
+               <com:Geslacht>?</com:Geslacht>
+               <com:Postcode>?</com:Postcode>
+               <com:Huisnummer>?</com:Huisnummer>
+            </com:Jongere-->
+         </sig:VerifieerJongere>
+         <sig:NieuwSignaal>
+            <sig:SignaalType>4</sig:SignaalType>
+            <!--Optional:- ->
+            <sig:Einddatum>?</sig:Einddatum>-->
+         </sig:NieuwSignaal>
+      </sig:nieuweSignalering>
+   </soapenv:Body>
+</soapenv:Envelope>
     EOF
     
     # Set Headers

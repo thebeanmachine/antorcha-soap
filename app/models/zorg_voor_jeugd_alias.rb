ff9e3ad5579e20c2f92536e93f894ec0e34cbb14
Daan Oosterveld <daan@dusdanig.com>
2010-11-22 16:22:12 +0100

    Added contract for zvj soap identity translation.

--- app/models/zorg_voor_jeugd_alias.rb
+++ app/models/zorg_voor_jeugd_alias.rb
@@ -8,11 +8,17 @@ class ZorgVoorJeugdAlias < ActiveRecord::Base
   validates_presence_of :organisatie_uuid, :unless => :organisatie_naam_and_postcode?,
     :message => 'moet opgegeven zijn als er geen naam en postcode is opgegeven'
 
+  def self.lookup_alias organization_id, organization_username
+    find :first, :conditions => { :organization_id => organization_id, :organization_username => organization_username }
+  end
+
+  def organization
+    @organization ||= Organization.find(organization_id)
+  end
+
+private
   def organisatie_naam_and_postcode?
     organisatie_naam? && organisatie_postcode?
   end
   
-  def organization
-    @organization ||= Organization.find(organization_id)
-  end
 end
--- spec/models/zorg_voor_jeugd_alias_spec.rb
+++ spec/models/zorg_voor_jeugd_alias_spec.rb
@@ -15,4 +15,31 @@ describe ZorgVoorJeugdAlias do
   it "should create a new instance given valid attributes" do
     ZorgVoorJeugdAlias.create!(@valid_attributes)
   end
+  
+  it "should be able to lookup an alias" do
+    @alias = ZorgVoorJeugdAlias.create! \
+      :organization_id => 123,
+      :organization_username => "jan_melder",
+      :gebruikersnaam_medewerker => "value for gebruikersnaam_medewerker",
+      :organisatie_naam => "value for organisatie_naam",
+      :organisatie_postcode => "value for organisatie_postcode",
+      :organisatie_uuid => "value for organisatie_uuid"
+
+    result = ZorgVoorJeugdAlias.lookup_alias 123, 'jan_melder'
+    result.should == @alias
+  end
+
+  it "should be able to lookup an alias and not find anything" do
+    @alias = ZorgVoorJeugdAlias.create! \
+      :organization_id => 123,
+      :organization_username => "jan_melder",
+      :gebruikersnaam_medewerker => "value for gebruikersnaam_medewerker",
+      :organisatie_naam => "value for organisatie_naam",
+      :organisatie_postcode => "value for organisatie_postcode",
+      :organisatie_uuid => "value for organisatie_uuid"
+
+    result = ZorgVoorJeugdAlias.lookup_alias 123, 'jan_smelter'
+    result.should == nil
+  end
+
 end

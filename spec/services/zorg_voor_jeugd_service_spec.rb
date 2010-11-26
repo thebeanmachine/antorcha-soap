1e9f51f90c2e0789313a372c4c4fbc94dda304d2
Robin Janse <r.janse@thebeanmachine.nl>
2010-11-26 13:58:58 +0100

    Zorg voor jeugd service unit test

--- spec/services/zorg_voor_jeugd_service_spec.rb
+++ spec/services/zorg_voor_jeugd_service_spec.rb
@@ -9,10 +9,6 @@ describe ZorgVoorJeugdService do
   def message_body
    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<nieuwe-signalering>\n  <signaaltype>4</signaaltype>\n  <huisnummer>6</huisnummer>\n  <einddatum></einddatum>\n  <postcode>5754DE</postcode>\n  <achternaam>Monroe</achternaam>\n  <bsn>73961486</bsn>\n  <geslacht>VROUW</geslacht>\n  <geboortedatum>1996-06-01</geboortedatum>\n</nieuwe-signalering>\n"
   end
-  
-  def body
-    message_body["nieuwe-signalering"].symbolize_keys!
-  end
  
   subject {
     mock_message.stub \
@@ -21,10 +17,8 @@ describe ZorgVoorJeugdService do
   } 
    
   describe "nieuwe signalering" do
-
    
-   it "should work" do   
-    
+   it "should reply with success" do    
     
     response = mock(ZorgVoorJeugd::Response)
     response.stub(:success?).and_return(true)
@@ -36,7 +30,34 @@ describe ZorgVoorJeugdService do
 
     ZorgVoorJeugd::Base.stub :new => signalering
     
-    #signalering.should_recieve(:create).with(:body => {:test => "sdfs"}).and_return(true)
+    subject.nieuwe_signalering
+    
+    subject.replies.each do |r|
+     puts "-" * 80
+     puts r.step_symbol
+     puts r.title
+     puts r.body.inspect
+     puts r.xml_serialized_body
+     r.title.should == "Gesignaleerd"
+     r.step_symbol.should == :antwoordbericht_nieuwe_signalering
+     r.body[:nieuwe_signalering][:status_code].should == 99
+    end
+    
+   end
+   
+   it "should reply with a warning" do    
+    
+    response = mock(ZorgVoorJeugd::Response)
+   
+   response.stub(:success?).and_return(false)
+    response.stub(:warning?).and_return(true)
+    
+    response.stub :status_code => 99, :status_omschrijving => 'Whoeps'
+    
+    signalering = mock(ZorgVoorJeugd::Base)
+    signalering.stub :create => response
+
+    ZorgVoorJeugd::Base.stub :new => signalering
     
     subject.nieuwe_signalering
     
@@ -46,6 +67,33 @@ describe ZorgVoorJeugdService do
      puts r.title
      puts r.body.inspect
      puts r.xml_serialized_body
+     r.title.should == "Gesignaleerd, echter met een waarschuwing"
+     r.step_symbol.should == :antwoordbericht_nieuwe_signalering
+     r.body[:nieuwe_signalering][:waarschuwing].should == true
+    end
+    
+   end
+   
+   it "should reply with an failure of some kind" do    
+    
+    response = mock(ZorgVoorJeugd::Response)
+   
+   response.stub(:success?).and_return(false)
+    response.stub(:warning?).and_return(false)
+    
+    response.stub :status_code => 99, :status_omschrijving => 'Whoeps'
+    
+    signalering = mock(ZorgVoorJeugd::Base)
+    signalering.stub :create => response
+
+    ZorgVoorJeugd::Base.stub :new => signalering
+    
+    subject.nieuwe_signalering
+    
+    subject.replies.each do |r|
+     r.title.should == "Signalering mislukt"
+     r.step_symbol.should == :antwoordbericht_nieuwe_signalering
+     r.body[:nieuwe_signalering][:failure].should == true
     end
     
    end 

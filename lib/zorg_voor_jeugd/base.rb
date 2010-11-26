471a2ad2fd84be6b98d22affd945bb25d13f1586
Daan Oosterveld <daan@dusdanig.com>
2010-11-26 15:36:27 +0100

    Added wijzig signalering to zorg voor jeugd.

--- lib/zorg_voor_jeugd/base.rb
+++ lib/zorg_voor_jeugd/base.rb
@@ -51,9 +51,41 @@ module ZorgVoorJeugd
       end
     end
     
-    def update
+    def update signalering = {}
       # gevonden signaal uuid: 97523
+      client = Savon::Client.new 'http://zvjtest.interaccess.nl/zvj-ws-v2/services/SignaleringWebService/'
+      result = client.wijzig_signalering! do |soap|
+        soap.namespace = 'http://zvj.interaccess.nl/signalering/'
+
+        xml = Builder::XmlMarkup.new :indent => 2
+        
+        xml.wsdl :OrganisatieGebruiker, 'xmlns:com' => "http://zvj.interaccess.nl/common/" do
+          if organisatie_naw[:uuid]
+            xml.com :OrganisatieUUID, organisatie_naw[:uuid]
+          else
+            xml.com :OrganisatieNAW do
+              xml.com :OrganisatieNaam, organisatie_naw[:naam]
+              xml.com :OrganisatiePostcode, organisatie_naw[:postcode]
+            end
+          end
+          xml.com :GebruikernaamMedewerker, organisatie_naw[:username]
+        end
+        xml.wsdl :WijzigSignaal do
+          xml.wsdl :SignaalUUID, signalering[:signaal_uuid]
+          xml.wsdl :Einddatum, signalering[:einddatum]
+        end
+
+        soap.body = xml.target!
+      end
       
+      unless result.soap_fault? or result.http_error?
+        hash = result.to_hash
+        puts hash.inspect
+        Response.new(hash[:wijzig_signalering_response])
+      else
+        return Response.new(:status_code => 99, :omschrijving => result.soap_fault.to_s) if result.soap_fault?
+        return Response.new(:status_code => 99, :omschrijving => result.http_error.to_s)if result.http_error?
+      end
     end
     
   end
--- spec/lib/zorg_voor_jeugd/base_spec.rb
+++ spec/lib/zorg_voor_jeugd/base_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+describe ZorgVoorJeugd::Base do    
+
   def organisatie_naw # *
     {:naam => 'Thorax', :postcode => '3800AD', :username => 'thebeanmachine'}
   end
@@ -28,52 +30,62 @@ require 'spec_helper'
     jongere_naw_required_fields.merge signaaltype
   end
   
-  describe ZorgVoorJeugd::Base do    
+  subject { ZorgVoorJeugd::Base.new organisatie_naw }
     
-    describe "create" do
-      context "with valid 'organisatie_naw'" do
-        
-        subject { ZorgVoorJeugd::Base.new organisatie_naw }
-        
-        context "and with an minimum of the required and valid fields" do
-          it "should create and match the status code '0' or '39'" do
-            result = subject.create required_fields
-            result.status_code.should match(/^(0|39)$/)
-          end
-          
-          it "should create a signalering based on a bsn" do
-            result = subject.create(bsn.merge signaaltype)
-            result.status_code.should match(/^(0|39)$/)
-          end
-        end
-        
-        it "should create a signalering" do
-          result = subject.create(jongere_naw_required_fields.merge(jongere_naw_optional_fields).merge(signaaltype).merge(einddatum))
+  describe "create" do
+    context "with valid 'organisatie_naw'" do
+      
+      context "and with an minimum of the required and valid fields" do
+        it "should create and match the status code '0' or '39'" do
+          result = subject.create required_fields
           result.status_code.should match(/^(0|39)$/)
-        end    
-        
-        context "and with invalid fields" do
-          it "should warn about a silly bsn" do
-            result = subject.create({ :bsn => '123456789', :signaaltype => '1' })
-            result.should be_failure
-          end
-          
-          it "should raise an error about a wrong date" do
-            result = subject.create required_fields.merge(:einddatum => "12-21-2012")
-            result.response[:status_code].should == 99
-          end
         end
-  
+        
+        it "should create a signalering based on a bsn" do
+          result = subject.create(bsn.merge signaaltype)
+          result.status_code.should match(/^(0|39)$/)
+        end
       end
       
-      context "with invalid 'organisatie_naw'" do
-        subject { ZorgVoorJeugd::Base.new :naam => 'Foo', :postcode => '0000AA', :username => 'foo-user' }
+      it "should create a signalering" do
+        result = subject.create(jongere_naw_required_fields.merge(jongere_naw_optional_fields).merge(signaaltype).merge(einddatum))
+        result.status_code.should match(/^(0|39)$/)
+      end    
+      
+      context "and with invalid fields" do
+        it "should warn about a silly bsn" do
+          result = subject.create({ :bsn => '123456789', :signaaltype => '1' })
+          result.should be_failure
+        end
         
-        it "should raise status code 99" do
-          result =  subject.create required_fields
+        it "should raise an error about a wrong date" do
+          result = subject.create required_fields.merge(:einddatum => "12-21-2012")
           result.response[:status_code].should == 99
         end
       end
+
     end
     
+    context "with invalid 'organisatie_naw'" do
+      subject { ZorgVoorJeugd::Base.new :naam => 'Foo', :postcode => '0000AA', :username => 'foo-user' }
+      
+      it "should raise status code 99" do
+        result =  subject.create required_fields
+        result.response[:status_code].should == 99
+      end
+    end
   end
+
+  describe "update" do
+    it "should change the only valid uuid we know" do
+      result = subject.update :signaal_uuid => '97523'
+      result.status_code.should == '0'
+    end
+
+    it "should change the only valid uuid we to a new einddatum" do
+      result = subject.update :signaal_uuid => '97523', :einddatum => '2012-01-01'
+      result.status_code.should == '0'
+    end
+  end
+  
+end

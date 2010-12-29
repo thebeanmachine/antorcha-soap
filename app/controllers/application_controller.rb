2098d13c73f1505c979e28886a10135eb856b1bb
Robin Janse <r.janse@thebeanmachine.nl>
2010-12-29 17:21:29 +0100

    Capitalize antorcha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,18 @@ class ApplicationController < ActionController::Base
   # filter_parameter_logging :password
 
   rescue_from "AntorchaConfigurationMissing" do
-    redirect_to new_antorcha_url, :notice => "Configureer eerst een antorcha locatie."
+    redirect_to new_antorcha_url, :notice => "Configureer eerst een Antorcha locatie."
   end
   
   rescue_from "ActiveResource::UnauthorizedAccess" do
-    redirect_to edit_antorcha_url(Antorcha.instance), :notice => "Er kon geen verbinding gemaakt worden met de antorcha omdat de opgegeven naam en wachtwoord geen toegang heeft tot het systeem."
+    redirect_to edit_antorcha_url(Antorcha.instance), :notice => "Er kon geen verbinding gemaakt worden met de Antorcha omdat de opgegeven naam en wachtwoord geen toegang heeft tot het systeem."
   end
   
   rescue_from "ActiveResource::Redirection" do
-    redirect_to edit_antorcha_url(Antorcha.instance), :notice => "Er kon geen verbinding gemaakt worden met de antorcha omdat er geen inloggegevens zijn opgegeven."
+    redirect_to edit_antorcha_url(Antorcha.instance), :notice => "Er kon geen verbinding gemaakt worden met de Antorcha omdat er geen inloggegevens zijn opgegeven."
   end
   
   rescue_from "Errno::ECONNREFUSED" do
-    redirect_to edit_antorcha_url(Antorcha.instance), :notice => "Er kon geen verbinding gemaakt worden met de antorcha omdat de connectie werd geweigerd."
+    redirect_to edit_antorcha_url(Antorcha.instance), :notice => "Er kon geen verbinding gemaakt worden met de Antorcha omdat de connectie werd geweigerd."
   end
 end
